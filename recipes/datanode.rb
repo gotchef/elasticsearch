cf27deeb4e2f404568b05bab2b165cd99d766a22
Franklin Wise <franklin@krave.io>
2015-08-19 22:24:54 -0700

    typo

--- recipes/datanode.rb
+++ recipes/datanode.rb
@@ -5,4 +5,4 @@ node.override[:elasticsearch][:node][:master] = false
 node.override[:elasticsearch][:http][:enabled] = false
 node.override[:elasticsearch][:node][:data] = true
 
-include_recipe 'elasticsearch-cave::insta
+include_recipe 'elasticsearch-cave::install'
